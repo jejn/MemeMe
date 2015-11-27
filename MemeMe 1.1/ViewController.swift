1ecb3f70ba3838edbd828cc57274038b8b21af4a
jejn <jimnording@gmail.com>
2015-11-27 17:33:37 +0400

    Add Outlets and Actions

--- MemeMe 1.1/ViewController.swift
+++ MemeMe 1.1/ViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // Outlets
+    
+    @IBOutlet weak var memeView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    
+    
+    
+    // Actions
+    
+    @IBAction func accessCamera(sender: AnyObject) {
+    }
+    
+    @IBAction func accessPhotoAlbum(sender: AnyObject) {
+    }
+    
+    @IBAction func shareMeme(sender: AnyObject) {
+    }
+
+    @IBAction func cancelAction(sender: AnyObject) {
     }
 
 
